b6ffaf940a9327a1ccce62e34b040d180e772c0c
Vladimir Burdukov <chippcheg@gmail.com>
2018-01-18 16:21:39 +0300

    Reverted PLoader, added PContextLoader

--- Sources/Loaders/PlainLoader.swift
+++ Sources/Loaders/PlainLoader.swift
@@ -3,6 +3,18 @@ import RxSwift
 
 public protocol PLoader: class {
   associatedtype PLResult: OptionalResult
+
+  func request(for loadingIntent: LoaderIntent) throws -> Request<PLResult>
+  func sections(from result: PLResult, loadingIntent: LoaderIntent) -> [Sectionable]?
+  func didReceive(result: PLResult, loadingIntent: LoaderIntent)
+}
+
+public extension PLoader {
+  func didReceive(result: PLResult, loadingIntent: LoaderIntent) {}
+}
+
+public protocol PContextLoader: class {
+  associatedtype PLResult: OptionalResult
   associatedtype Context
 
   func request(for loadingIntent: LoaderIntent, context: Context) throws -> Request<PLResult>
@@ -10,15 +22,41 @@ public protocol PLoader: class {
   func didReceive(result: PLResult, loadingIntent: LoaderIntent, context: Context)
 }
 
-public extension PLoader {
+public extension PContextLoader {
   func didReceive(result: PLResult, loadingIntent: LoaderIntent, context: Context) {}
 }
 
-public func load<T: PLoader>(pLoader loader: T, intent: LoaderIntent) -> SectionObservable where T.Context == Void {
-  return load(pLoader: loader, intent: intent, context: ())
+public func load<T: PLoader>(pLoader loader: T, intent: LoaderIntent) -> SectionObservable {
+  do {
+    let request = try loader.request(for: intent)
+    let observable: Observable<Response<T.PLResult>>
+    switch intent {
+    case .page, .autoupdate, .pullToRefresh:
+      observable = Gnomon.models(for: request)
+    case let .force(keepData) where keepData:
+      observable = Gnomon.models(for: request)
+    default:
+      observable = Gnomon.cachedThenFetch(request)
+    }
+    return observable.flatMap { [weak loader] response -> SectionObservable in
+      switch (intent, response.responseType) {
+      case (.page, _), (.autoupdate, _), (.pullToRefresh, _): break
+      case (.force(let keepData), _) where keepData: break
+      case (_, .httpCache): return .empty()
+      default: break
+      }
+
+      return Observable.just(loader?.sections(from: response.result, loadingIntent: intent)).do(onNext: { _ in
+        loader?.didReceive(result: response.result, loadingIntent: intent)
+      }).subscribeOn(MainScheduler.instance)
+    }.subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default)).observeOn(MainScheduler.instance)
+  } catch {
+    return .error(error)
+  }
+
 }
 
-public func load<T: PLoader>(pLoader loader: T, intent: LoaderIntent, context: T.Context) -> SectionObservable {
+public func load<T: PContextLoader>(pLoader loader: T, intent: LoaderIntent, context: T.Context) -> SectionObservable {
   do {
     let request = try loader.request(for: intent, context: context)
     let observable: Observable<Response<T.PLResult>>
